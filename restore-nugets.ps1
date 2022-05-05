957635506aa22b3cbdfb91389409eea741f8b6f9
Michal Korniak <michal.korniak@kornit.com>
2022-05-05 15:21:11 +0200

    restore-nugets script

--- restore-nugets.ps1
+++ restore-nugets.ps1
@@ -1,3 +1,24 @@
 $local:solutionDirectoryPath = "C:\Users\michal.korniak\source\repos\KRIP"
-$local:libraryDirectoryPath = "C:\Users\michal.korniak\source\repos\Infrastructure"
+
+function LogMessage($message) {
+    $currentTime = Get-Date -format "dd-MMM-yyyy HH:mm:ss"
+    Write-Output ('[' + $currentTime + '] ' + $message)    
+}
+
+function RestoreFileBackup([string]$filePath) {
+    $local:backupFilePath = ($filePath + '.backup')
+    Copy-Item -Path $backupFilePath -Destination $filePath
+    Remove-Item $backupFilePath
+    LogMessage ($filePath + ' backup file restored')
+}
+
+
+
+$local:slnPath = (Get-ChildItem $SolutionDirectoryPath -Recurse *.sln).FullName
+RestoreFileBackup $slnPath
+
+$local:projectsPaths = (Get-ChildItem $SolutionDirectoryPath -Recurse *.csproj).FullName
+foreach ($projectPath in $projectsPaths) {
+    RestoreFileBackup $projectPath
+}
 
